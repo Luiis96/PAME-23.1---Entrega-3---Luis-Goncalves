e4e982efd3c0e690b1b9f7150be638b5a2885b37
Luís <luisrobertoloureiro@gmail.com>
2023-07-10 23:57:51 -0300

    Entrega

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Componentes/Header/Topo";
+import Content from "./Componentes/Content1/Content.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Content></Content>
+    </div>
+  );
+}
+
+export default App;
